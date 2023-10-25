493f755b0d4c27610bd9379abf54c8d95b462664
asscs15 <62126544+asscs15@users.noreply.github.com>
2023-10-25 14:18:52 -0600

    Delete Olympia directory
    No sirve

--- Olympia/conection/con_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Conexion con la base de datos en este caso para el index las demas funciones tendran su conexion en su carpeta correspondiente
-$conex = mysqli_connect("localhost","root","","servicio_social"); 
-
-?>
--- Olympia/modificaciones/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","root","","servicio_social");
-
-if($mysqli -> connect_errno){
-    echo 'Fallo en la conexion' . mysqli -> connect_error;
-    die(); 
-}
-
-?>
